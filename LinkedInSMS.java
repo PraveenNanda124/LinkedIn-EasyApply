04b7630ee136929f3ff3f2ccefaee1b898980d35
ebowwa <81942069+ebowwa@users.noreply.github.com>
2022-12-09 02:37:35 -0800

    Update LinkedInSMS.java

--- LinkedInSMS.java
+++ LinkedInSMS.java
@@ -1,39 +1,24 @@
-public class LinkedInEasyApply {
+public class LinkedInSMS {
 public static void main(String[] args) throws IOException {
-// Set up LinkedInLogin object with login credentials
-LinkedInLogin login = new LinkedInLogin("your_username", "your_password", "your_phone_number");
-  
-    // Set up LinkedInSMS object with phone number
-    LinkedInSMS sms = new LinkedInSMS("your_phone_number");
+// Set up LinkedInSMS object with phone number
+LinkedInSMS sms = new LinkedInSMS("your_phone_number");
 
-    // Set up LinkedInSearch object with search keywords and filters
-    LinkedInSearch search = new LinkedInSearch("your_keywords", "your_location", "your_job_title", "your_company", "your_experience_level", "your_job_type");
-
-    // Set up LinkedInShare object with job url and connections
-    LinkedInShare share = new LinkedInShare("your_job_url", ["your_connection_1", "your_connection_2"], "your_message");
-
-    // Set up ChromeDriver
+      // Set up ChromeDriver
     System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--incognito");
     WebDriver driver = new ChromeDriver(options);
-
-    // Log in to LinkedIn
-    try {
-        login.login(driver);
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
 
     // Handle SMS verification
     try {
         sms.handleSMSVerification(driver);
     } catch (IOException e) {
         e.printStackTrace();
+    } catch {
+        // Handle any exceptions
     }
 
-    // Search for job postings
-    try {
-        search.search(driver);
-    } catch (IOException e) {
-        e.printStackTr
+    // Close the WebDriver instance
+    driver.quit();
+}
+}
