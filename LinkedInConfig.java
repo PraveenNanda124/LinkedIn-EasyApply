e193479f40b3028511fce2aecac001a17fd4df3c
ebowwa <81942069+ebowwa@users.noreply.github.com>
2022-12-09 02:14:24 -0800

    Update LinkedInConfig.java

--- LinkedInConfig.java
+++ LinkedInConfig.java
@@ -1,1 +1,194 @@
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class LinkedInConfig {
+// Set up login credentials
+String username;
+String password;
+String phoneNumber;
+
+// Set up ChromeDriver
+System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--incognito");
+WebDriver driver = new ChromeDriver(options);
+
+public LinkedInConfig(String username, String password, String phoneNumber) {
+    this.username = username;
+    this.password = password;
+    this.phoneNumber = phoneNumber;
+}
+
+public void readProperties(String fileName) {
+    Properties prop = new Properties();
+    InputStream input = null;
+
+    try {
+        input = new FileInputStream(fileName);
+        prop.load(input);
+
+        // Set login credentials
+        this.username = prop.getProperty("username");
+        this.password = prop.getProperty("password");
+        this.phoneNumber = prop.getProperty("phoneNumber");
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        if (input != null) {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public void setUsername(String username) {
+    this.username = username;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+}
+
+public String getUsername() {
+    return this.username;
+}
+
+public String getPassword() {
+    return this.password;
+}
+
+public String getPhoneNumber() {
+    return this.phoneNumber;
+}
+// Set up ChromeDriver
+System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--incognito");
+WebDriver driver = new ChromeDriver(options);
+
+public LinkedInConfig(String username, String password, String phoneNumber) {
+    this.username = username;
+    this.password = password;
+    this.phoneNumber = phoneNumber;
+}
+
+public void readProperties(String fileName) {
+    Properties prop = new Properties();
+    InputStream input = null;
+
+    try {
+        input = new FileInputStream(fileName);
+        prop.load(input);
+
+        // Set login credentials
+        this.username = prop.getProperty("username");
+        this.password = prop.getProperty("password");
+        this.phoneNumber = prop.getProperty("phoneNumber");
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        if (input != null) {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public void setUsername(String username) {
+    this.username = username;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+}
+
+public String getUsername() {
+    return this.username;
+}
+
+public String getPassword() {
+    return this.password;
+}
+
+public String getPhoneNumber() {
+    return this.phoneNumber;
+}
+// Set up ChromeDriver
+System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--incognito");
+WebDriver driver = new ChromeDriver(options);
+
+public LinkedInConfig(String username, String password, String phoneNumber) {
+    this.username = username;
+    this.password = password;
+    this.phoneNumber = phoneNumber;
+}
+
+public void readProperties(String fileName) {
+    Properties prop = new Properties();
+    InputStream input = null;
+
+    try {
+        input = new FileInputStream(fileName);
+        prop.load(input);
+
+        // Set login credentials
+        this.username = prop.getProperty("username");
+        this.password = prop.getProperty("password");
+        this.phoneNumber = prop.getProperty("phoneNumber");
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        if (input != null) {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public void setUsername(String username) {
+    this.username = username;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+}
+
+public String getUsername() {
+    return this.username;
+}
+
+public String getPassword() {
+    return this.password;
+}
+
+public String getPhoneNumber() {
+    return this.phoneNumber;
+}
+}
